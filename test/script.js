2dd69bffd5e26af1b59cdb280cc9029124087426
kjschwoerke <kjschwoerke@yahoo.com>
2020-08-22 14:49:58 -0500

    cleaned up and commented code

--- test/script.js
+++ test/script.js
@@ -1,27 +1,13 @@
-// const { type } = require('os');
-
-const { type } = require('os');
-
-require('dotenv').config()
-
-let webdriver = require('selenium-webdriver'), 
-assert = require('assert'),
-username = process.env.SAUCE_USERNAME,
-accessKey = process.env.SAUCE_ACCESS_KEY,
+let webdriver = require('selenium-webdriver'),
 baseUrl = "https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_js_dropdown_hover"
 
-
-
-
-
 //##############################################################################################
-//Selenium should get the link in a drop down menu when active
+//Should get the link#2 in the drop down menu when active
 //##############################################################################################
 describe('Find & Access Drop Down Menu List Item', function() {
   this.timeout(40000)
   let driver
 
-  
   beforeEach(async function() {
     driver = new webdriver.Builder()
     .forBrowser('firefox')
@@ -29,26 +15,21 @@ describe('Find & Access Drop Down Menu List Item', function() {
   })
   
   afterEach(async function() {
-    // await driver.executeScript("sauce:job-result=" + (this.currentTest.state));
     await driver.quit();
   })
   
 
     it('find item',async function(){
-      await driver.get(baseUrl)
-      const iframe =  await driver.findElement({css:'#container > #iframecontainer > #iframe > #iframewrapper > iframe'})
-      await driver.switchTo().frame(iframe)
-      const button = await driver.findElement({css: 'button'})
-      button.click()
-      const actions = driver.actions()
-      await actions.move({origin: button}).perform()
-      let link = await driver.findElement({css: '.dropdown > .dropdown-content > a:nth-of-type(2)'})
-      await actions.move({origin: link}).perform()
-      link.click()
-        // assert(
-        //   button.isDisplayed(),
-        //   link.isDisplayed()
-        // )
+      await driver.get(baseUrl) //go to the website
+      const iframe =  await driver.findElement({css:'#container > #iframecontainer > #iframe > #iframewrapper > iframe'}) //find the iframe containing the button
+      await driver.switchTo().frame(iframe) //switch to the iframe to access inside content/elements
+      const button = await driver.findElement({css: 'button'}) //find the button inside the iframe
+      //button.click()
+      const actions = driver.actions() //initiate dirver.actions()
+      await actions.move({origin: button}).perform() //use the move action to move the mouse to the button
+      let link = await driver.findElement({css: '.dropdown > .dropdown-content > a:nth-of-type(2)'}) //find the 'Link #2' inside the revealed dropdown menu
+      await actions.move({origin: link}).perform() // move the mouse to link
+      //link.click()
     });
 
 });
